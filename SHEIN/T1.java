1db86daf39682a5823b80c3c766a0dfa72d704fd
ak918xp <ak918xp@126.com>
2021-07-16 16:15:52 +0800

    update:SHEIN笔试题

---
+++ SHEIN/T1.java
@@ -0,0 +1,21 @@
+public class T1 {
+    public static void main(String[] args){
+        String str ="a  b    c";
+        String res=changespaceto20(str);
+        System.out.println(res);
+    }
+    public static String changespaceto20 (String str) {
+        // write code here
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c==' '){
+                sb.append("%20");
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
